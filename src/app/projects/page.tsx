daf6ad8b937cd5336a5414083f057d83817cecd0
st3phable <berd2k@pm.me>
2025-12-07 23:17:16 -0500

    Delete the projects place to fix git cache

--- src/app/projects/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-  return (
-    <div className="min-h-screen p-24">
-      <h1 className="text-4xl font-bold mb-6">My Projects</h1>
-      <p>Project list coming soon...</p>
-    </div>
-  );
-}

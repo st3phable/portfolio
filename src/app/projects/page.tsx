142e2258545ec825ebc06e786708cf8cb33d6e80
st3phable <berd2k@pm.me>
2025-12-07 23:33:17 -0500

    Reset projects folder

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,7 @@
 export default function Projects() {
   return (
-    <div className='min-h-screen p-24'>
-      <h1 className='text-4xl font-bold mb-6'>My Projects</h1>
-      <p>Project list coming soon...</p>
+    <div>
+      <h1>My Projects</h1>
     </div>
   );
 }

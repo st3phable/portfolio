85e466c89548ed24e1d3cb0365a5a796504659c2
st3phable <berd2k@pm.me>
2026-01-29 16:37:43 -0500

    Add analytics and update fonts

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,25 +3,26 @@ import { HiArrowRight } from 'react-icons/hi';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white p-8">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#a477e1] text-white p-8">
       
       {/* Main Content Container */}
       <div className="text-center max-w-3xl">
 
         {/* 1. Headline */}
-        <h1 className="text-5xl md:text-7xl font-bold mb-4">
-          Hello, I'm Stephanie
+        <h1 className="text-5xl md:text-7xl --font-bold mb-4">
+          Hello! I'm Stephanie
         </h1>
 
         {/* 2. Sub-headline */}
-        <p className="text-xl md:text-2xl text-gray-300 mb-8">
-          I'm a Computer Scientist & Academic Researcher building for the web.
+        <p className="text-xl md:text-2xl text-white-200 mb-8">
+          I am a computer scientist & neuroscience esearcher.
+          My aim is to leverage computer-brain interfaces and related technology in order to find a cure for schizophrenia.
         </p>
 
         {/* 3. Call-to-Action (CTA) Button */}
         <a
           href="/projects" // This will link to src/app/projects/page.tsx
-          className="inline-flex items-center gap-2 bg-blue-600 text-white font-bold py-3 px-8 rounded-full text-lg hover:bg-blue-700 transition-colors duration-300"
+          className="inline-flex items-center gap-2 bg-[#8b55d0] text-white --font-bold py-3 px-8 rounded-full text-lg hover:bg-[#7e2eea] transition-colors duration-300"
         >
           View My Work
           <HiArrowRight/>
@@ -33,7 +34,7 @@ export default function Home() {
             href="https://github.com/st3phable" // Add your GitHub link
             target="_blank"
             rel="noopener noreferrer"
-            className="text-gray-400 hover:text-white transition-colors duration-300"
+            className="text-white-400 hover:text-white transition-colors duration-300"
             aria-label="GitHub"
           >
             <FaGithub size={32} />
@@ -43,7 +44,7 @@ export default function Home() {
             href="https://linkedin.com/in/stephanie-rosales-diaz-8a5558172/" // Add your LinkedIn link
             target="_blank"
             rel="noopener noreferrer"
-            className="text-gray-400 hover:text-white transition-colors duration-300"
+            className="text-white-400 hover:text-white transition-colors duration-300"
             aria-label="LinkedIn"
           >
             <FaLinkedin size={32} />

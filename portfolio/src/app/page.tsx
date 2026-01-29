42bd3c235cfb255472636f6ed4fb6226b891e713
st3phable <berd2k@pm.me>
2026-01-29 18:04:01 -0500

    Change headline to Space Mono and reduce size

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="text-center max-w-3xl">
 
         {/* 1. Headline */}
-        <h1 className="text-5xl md:text-7xl --font-bold mb-4">
+        <h1 className="text-4xl md:text-6xl font-mono font-bold mb-4">
           Hello! I'm Stephanie
         </h1>
 
